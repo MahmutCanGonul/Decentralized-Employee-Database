91cf8e8320de02ca4c0a621f7ecc1e1e03aa35c8
Mahmut Can Gönül <75094927+MahmutCanGonul@users.noreply.github.com>
2022-08-01 14:09:08 +0300

    Update Database.sol

--- Database.sol
+++ Database.sol
@@ -15,30 +15,33 @@ contract Database{
    }
     struct Company{
        uint256 id;
+       uint256 totalCoin;
        string companyAddress;
-       string companyName; 
+       string companyName;
+       uint256 timeStamp; 
    }
      uint256 companyCount=0;
      uint256 employeeCount=0;
-     Company[] companyData;
+     //Company[] companyData;
      mapping(uint256=>Company) public company;
      mapping(uint256=>Employee) public employee;
-     event CompanyCreated(uint256 id, string _companyAddress,string _companyName);
+     event CompanyCreated(uint256 id, string _companyAddress,string _companyName,uint256 _timestamp);
      event EmployeeCreated(bytes32 uid,string name,string companyAddress ,string phoneNumber,string location,uint256 timeStamp,string job,uint256 salary,string national);
     
      function CreatedCompany(string memory _companyAddress,string memory _companyName)public{
           require(bytes(_companyAddress).length > 1,"You must enter the cypto wallet address!");
           require(bytes(_companyName).length > 1,"You must enter the company name!");
           bool isAlreadyCreated=false;
-          for(uint i=0; i<companyData.length;i++){
-                  if(keccak256(bytes(_companyAddress)) == keccak256(bytes(companyData[i].companyAddress))){
+          for(uint i=0; i<companyCount;i++){
+                  if(keccak256(bytes(_companyAddress)) == keccak256(bytes(company[i].companyAddress))){
                       isAlreadyCreated = true;
                   }      
           }
           if(!isAlreadyCreated){
-                  company[companyCount] = Company(companyCount,_companyAddress,_companyName);
-                  emit CompanyCreated(companyCount,_companyAddress,_companyName);
-                  companyData.push(company[companyCount]);
+                  uint256 _time = block.timestamp;
+                  company[companyCount] = Company(companyCount,0,_companyAddress,_companyName,_time);
+                  emit CompanyCreated(companyCount,_companyAddress,_companyName,_time);
+                  //companyData.push(company[companyCount]);
                   companyCount++;
           }
      }
@@ -52,9 +55,9 @@ contract Database{
          require(bytes(_job).length > 1,"Enter a job!");
          require(bytes(_national).length > 1,"Enter a national!");
         
-         for(uint i=0; i<companyData.length;i++){
-             if(keccak256(bytes(_companyAddress)) == keccak256(bytes(companyData[i].companyAddress))){
-                    bytes32 _uid = keccak256(abi.encodePacked(block.timestamp));
+         for(uint i=0; i<companyCount;i++){
+             if(keccak256(bytes(_companyAddress)) == keccak256(bytes(company[i].companyAddress))){
+                    bytes32 _uid = keccak256(abi.encodePacked(employeeCount));
                     uint256 _time = block.timestamp; 
                     employee[employeeCount] = Employee(
                         employeeCount,
@@ -74,6 +77,25 @@ contract Database{
                     employeeCount++;
                     break;        
                }
+           }
+
+       }
+
+       function RemoveEmployee(string memory _companyAddress,uint256 _id)public{
+           for(uint i=0; i<companyCount;i++){
+                if(keccak256(bytes(company[i].companyAddress)) == keccak256(bytes(_companyAddress))){
+                     for(uint j=0; j<employeeCount;j++){
+                         if(employee[j].uid == keccak256(abi.encodePacked(_id))){
+                                 employee[j].companyAddress="***";
+                                 employee[j].phoneNumber="***";
+                                 employee[j].name="***";
+                                 employee[j].location="***";
+                                 employee[j].national="***";
+                                 employee[j].job="***";
+                                 break;
+                         }
+                     }
+                }
            }
        }
        
