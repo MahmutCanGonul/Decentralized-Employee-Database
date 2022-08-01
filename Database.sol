f06f55661d00cc29cd68c8abcbe9a6a512fa7254
Mahmut Can Gönül <75094927+MahmutCanGonul@users.noreply.github.com>
2022-08-01 17:17:20 +0300

    Update Database.sol

--- Database.sol
+++ Database.sol
@@ -5,6 +5,7 @@ contract Database{
        uint256 id;
        bytes32 uid;
        string name;
+       string employeeAddress;
        string companyAddress;
        string phoneNumber;
        string location;
@@ -25,8 +26,9 @@ contract Database{
      //Company[] companyData;
      mapping(uint256=>Company) public company;
      mapping(uint256=>Employee) public employee;
+     mapping(string=>Employee) public employeeFromCompany;
      event CompanyCreated(uint256 id, string _companyAddress,string _companyName,uint256 _timestamp);
-     event EmployeeCreated(bytes32 uid,string name,string companyAddress ,string phoneNumber,string location,uint256 timeStamp,string job,uint256 salary,string national);
+     event EmployeeCreated(bytes32 uid,string name,string employeeAddress,string companyAddress ,string phoneNumber,string location,uint256 timeStamp,string job,uint256 salary,string national);
     
      function CreatedCompany(string memory _companyAddress,string memory _companyName)public{
           require(bytes(_companyAddress).length == 42,"You must enter the cypto wallet address!");
@@ -46,7 +48,7 @@ contract Database{
           }
      }
     
-     function AddEmployee(string memory _name,string memory _companyAddress,string memory _phoneNumber,string memory _location,string memory _job,
+     function AddEmployee(string memory _name,string memory _employeeAddress,string memory _companyAddress,string memory _phoneNumber,string memory _location,string memory _job,
      uint256  _salary,string memory _national)public{
          require(bytes(_name).length > 1,"Enter a name!");
          require(bytes(_companyAddress).length > 1,"You must enter the cypto wallet address!");
@@ -63,6 +65,7 @@ contract Database{
                         employeeCount,
                         _uid,
                         _name,
+                        _employeeAddress,
                         _companyAddress,
                        _phoneNumber,
                        _location,
@@ -72,8 +75,8 @@ contract Database{
                        _national
 
                     );
-                    employee[employeeCount] = Employee(employeeCount,_uid,_name,_companyAddress,_phoneNumber,_location,_time,_job,_salary,_national);
-                    emit EmployeeCreated(_uid,_name,_companyAddress,_phoneNumber,_location,_time,_job,_salary,_national);
+                    employee[employeeCount] = Employee(employeeCount,_uid,_name,_employeeAddress,_companyAddress,_phoneNumber,_location,_time,_job,_salary,_national);
+                    emit EmployeeCreated(_uid,_name,_employeeAddress,_companyAddress,_phoneNumber,_location,_time,_job,_salary,_national);
                     employeeCount++;
                     break;        
                }
@@ -98,16 +101,21 @@ contract Database{
                 }
            }
        }
+        
+       function EmployeesNumberFromCompany(string memory _companyName)public view returns(uint256){
+           require(bytes(_companyName).length >1,"Enter the name of the company!");
+           uint256 _employeeCounter=0;
+           for(uint i=0; i<companyCount;i++){
+              if(keccak256(bytes(company[i].companyName)) == keccak256(bytes(_companyName))){
+                        for(uint j=0; j<employeeCount;j++){
+                            if(keccak256(bytes(employee[j].companyAddress)) == keccak256(bytes(company[i].companyAddress))){
+                                  _employeeCounter++;
+                            }
+                        }
+                        break;
+              }
+         }
+         return _employeeCounter;
+       }
        
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
    }
